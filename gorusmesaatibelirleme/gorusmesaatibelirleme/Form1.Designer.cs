f9a7d50d98d99ab9f6aa21303cd56ec87af48259
dgulsen <devrangulsenn@hotmail.com>
2019-06-11 16:10:21 +0300

    02.12.2013

---
+++ gorusmesaatibelirleme/gorusmesaatibelirleme/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace gorusmesaatibelirleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbx_gorusmeSure = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbx_baslangicSaat = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbx_bitisSaat = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbx_oglenbaslangicSaat = new System.Windows.Forms.ComboBox();
+            this.cmbx_oglenbitisSaat = new System.Windows.Forms.ComboBox();
+            this.cmbx_bitisDakika = new System.Windows.Forms.ComboBox();
+            this.cmbx_oglenbaslangicDakika = new System.Windows.Forms.ComboBox();
+            this.cmbx_baslangicDakika = new System.Windows.Forms.ComboBox();
+            this.cmbx_oglenbitisDakika = new System.Windows.Forms.ComboBox();
+            this.btn_olustur = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lstbx_gorusmeler = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Görüşme Süresi";
+            // 
+            // cmbx_gorusmeSure
+            // 
+            this.cmbx_gorusmeSure.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbx_gorusmeSure.FormattingEnabled = true;
+            this.cmbx_gorusmeSure.Location = new System.Drawing.Point(240, 46);
+            this.cmbx_gorusmeSure.Name = "cmbx_gorusmeSure";
+            this.cmbx_gorusmeSure.Size = new System.Drawing.Size(181, 24);
+            this.cmbx_gorusmeSure.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Görüşme Başlangıç Zamanı";
+            // 
+            // cmbx_baslangicSaat
+            // 
+            this.cmbx_baslangicSaat.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbx_baslangicSaat.FormattingEnabled = true;
+            this.cmbx_baslangicSaat.Location = new System.Drawing.Point(240, 83);
+            this.cmbx_baslangicSaat.Name = "cmbx_baslangicSaat";
+            this.cmbx_baslangicSaat.Size = new System.Drawing.Size(75, 24);
+            this.cmbx_baslangicSaat.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Öğlen Arası Başlangıç";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Öğlen Arası Bitiş";
+            // 
+            // cmbx_bitisSaat
+            // 
+            this.cmbx_bitisSaat.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbx_bitisSaat.FormattingEnabled = true;
+            this.cmbx_bitisSaat.Location = new System.Drawing.Point(240, 134);
+            this.cmbx_bitisSaat.Name = "cmbx_bitisSaat";
+            this.cmbx_bitisSaat.Size = new System.Drawing.Size(75, 24);
+            this.cmbx_bitisSaat.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(144, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Görüşme Bitiş Zamanı";
+            // 
+            // cmbx_oglenbaslangicSaat
+            // 
+            this.cmbx_oglenbaslangicSaat.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbx_oglenbaslangicSaat.FormattingEnabled = true;
+            this.cmbx_oglenbaslangicSaat.Location = new System.Drawing.Point(240, 177);
+            this.cmbx_oglenbaslangicSaat.Name = "cmbx_oglenbaslangicSaat";
+            this.cmbx_oglenbaslangicSaat.Size = new System.Drawing.Size(75, 24);
+            this.cmbx_oglenbaslangicSaat.TabIndex = 8;
+            // 
+            // cmbx_oglenbitisSaat
+            // 
+            this.cmbx_oglenbitisSaat.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbx_oglenbitisSaat.FormattingEnabled = true;
+            this.cmbx_oglenbitisSaat.Location = new System.Drawing.Point(240, 227);
+            this.cmbx_oglenbitisSaat.Name = "cmbx_oglenbitisSaat";
+            this.cmbx_oglenbitisSaat.Size = new System.Drawing.Size(75, 24);
+            this.cmbx_oglenbitisSaat.TabIndex = 9;
+            // 
+            // cmbx_bitisDakika
+            // 
+            this.cmbx_bitisDakika.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbx_bitisDakika.FormattingEnabled = true;
+            this.cmbx_bitisDakika.Location = new System.Drawing.Point(346, 134);
+            this.cmbx_bitisDakika.Name = "cmbx_bitisDakika";
+            this.cmbx_bitisDakika.Size = new System.Drawing.Size(75, 24);
+            this.cmbx_bitisDakika.TabIndex = 10;
+            // 
+            // cmbx_oglenbaslangicDakika
+            // 
+            this.cmbx_oglenbaslangicDakika.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbx_oglenbaslangicDakika.FormattingEnabled = true;
+            this.cmbx_oglenbaslangicDakika.Location = new System.Drawing.Point(346, 177);
+            this.cmbx_oglenbaslangicDakika.Name = "cmbx_oglenbaslangicDakika";
+            this.cmbx_oglenbaslangicDakika.Size = new System.Drawing.Size(75, 24);
+            this.cmbx_oglenbaslangicDakika.TabIndex = 11;
+            // 
+            // cmbx_baslangicDakika
+            // 
+            this.cmbx_baslangicDakika.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbx_baslangicDakika.FormattingEnabled = true;
+            this.cmbx_baslangicDakika.Location = new System.Drawing.Point(346, 83);
+            this.cmbx_baslangicDakika.Name = "cmbx_baslangicDakika";
+            this.cmbx_baslangicDakika.Size = new System.Drawing.Size(75, 24);
+            this.cmbx_baslangicDakika.TabIndex = 12;
+            // 
+            // cmbx_oglenbitisDakika
+            // 
+            this.cmbx_oglenbitisDakika.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbx_oglenbitisDakika.FormattingEnabled = true;
+            this.cmbx_oglenbitisDakika.Location = new System.Drawing.Point(346, 222);
+            this.cmbx_oglenbitisDakika.Name = "cmbx_oglenbitisDakika";
+            this.cmbx_oglenbitisDakika.Size = new System.Drawing.Size(75, 24);
+            this.cmbx_oglenbitisDakika.TabIndex = 13;
+            // 
+            // btn_olustur
+            // 
+            this.btn_olustur.Location = new System.Drawing.Point(240, 273);
+            this.btn_olustur.Name = "btn_olustur";
+            this.btn_olustur.Size = new System.Drawing.Size(183, 27);
+            this.btn_olustur.TabIndex = 14;
+            this.btn_olustur.Text = "Liste Oluştur";
+            this.btn_olustur.UseVisualStyleBackColor = true;
+            this.btn_olustur.Click += new System.EventHandler(this.btn_olustur_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(473, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Görüşme Saatleri";
+            // 
+            // lstbx_gorusmeler
+            // 
+            this.lstbx_gorusmeler.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstbx_gorusmeler.FormattingEnabled = true;
+            this.lstbx_gorusmeler.ItemHeight = 17;
+            this.lstbx_gorusmeler.Location = new System.Drawing.Point(448, 65);
+            this.lstbx_gorusmeler.Name = "lstbx_gorusmeler";
+            this.lstbx_gorusmeler.Size = new System.Drawing.Size(413, 174);
+            this.lstbx_gorusmeler.TabIndex = 16;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(887, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 229);
+            this.listBox1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1033, 359);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lstbx_gorusmeler);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btn_olustur);
+            this.Controls.Add(this.cmbx_oglenbitisDakika);
+            this.Controls.Add(this.cmbx_baslangicDakika);
+            this.Controls.Add(this.cmbx_oglenbaslangicDakika);
+            this.Controls.Add(this.cmbx_bitisDakika);
+            this.Controls.Add(this.cmbx_oglenbitisSaat);
+            this.Controls.Add(this.cmbx_oglenbaslangicSaat);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbx_bitisSaat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbx_baslangicSaat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbx_gorusmeSure);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Constantia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Görüşme Saati Belirleme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbx_gorusmeSure;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbx_baslangicSaat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbx_bitisSaat;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbx_oglenbaslangicSaat;
+        private System.Windows.Forms.ComboBox cmbx_oglenbitisSaat;
+        private System.Windows.Forms.ComboBox cmbx_bitisDakika;
+        private System.Windows.Forms.ComboBox cmbx_oglenbaslangicDakika;
+        private System.Windows.Forms.ComboBox cmbx_baslangicDakika;
+        private System.Windows.Forms.ComboBox cmbx_oglenbitisDakika;
+        private System.Windows.Forms.Button btn_olustur;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox lstbx_gorusmeler;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
